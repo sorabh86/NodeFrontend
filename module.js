320ef743b03f02fffa507642b93d18ae64904f62
Sorabh <ssorabh.ssharma@gmail.com>
2017-07-28 16:28:57 +0530

    js-yaml workaround

--- module.js
+++ module.js
@@ -1,5 +1,9 @@
-var fs = require('fs');
+var fs = require('fs'); // nodejs core file stream package
+var yaml = require("js-yaml"); // encode yaml file into object so you can access independent values
 
-var profile = fs.readFileSync("./profile.yml","utf8");
+var profile = yaml.safeLoad(fs.readFileSync("./profile.yml","utf8"));
+var message = profile.name + " is " + profile.age + " years old and likes " + profile.movies.join(" and ") + ".";
 
-console.log(profile);
+fs.writeFileSync("./profiles/mel-jones.txt", message, "utf8")
+
+console.log(profile.name);
