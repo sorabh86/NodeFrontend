1c6bc1d8ea602dcbbad4fdda7ca21e4ec4cd8166
Sorabh <ssorabh.ssharma@gmail.com>
2017-07-28 16:41:19 +0530

    core module practise

--- module.js
+++ module.js
@@ -7,4 +7,23 @@ var message = profile.name + " is " + profile.age + " years old and likes " + pr
 
 fs.writeFileSync(path.join(".","profiles","sos.txt"), message, "utf8")
 
-console.log(profile.name);
+// async file read
+fs.readFile("./profile.yml","utf8", function(err, file){
+
+	if(err) {
+		console.error(err);
+		return;
+	}
+
+	var profile = yaml.safeLoad(file);
+	var message = profile.name + " is " + profile.age + " years old and likes " + profile.movies.join(" and ") + ".";
+
+	var filepath = path.join(".","profiles","sos1.txt");
+	fs.writeFile(filepath,message,"utf8",function(err){
+		if(err) {
+			console.error(err);
+			return;
+		}
+		console.log("Profile for "+profile.name+" written to "+filepath);
+	});
+});
