1cd21788ecb8b9e02c940706ecb56a2506f7d8dd
Sorabh <ssorabh.ssharma@gmail.com>
2017-07-28 16:33:54 +0530

    core modules

--- module.js
+++ module.js
@@ -1,9 +1,10 @@
 var fs = require('fs'); // nodejs core file stream package
 var yaml = require("js-yaml"); // encode yaml file into object so you can access independent values
+var path = require("path"); // help you not to write slashes and more
 
 var profile = yaml.safeLoad(fs.readFileSync("./profile.yml","utf8"));
 var message = profile.name + " is " + profile.age + " years old and likes " + profile.movies.join(" and ") + ".";
 
-fs.writeFileSync("./profiles/mel-jones.txt", message, "utf8")
+fs.writeFileSync(path.join(".","profiles","sos.txt"), message, "utf8")
 
 console.log(profile.name);
